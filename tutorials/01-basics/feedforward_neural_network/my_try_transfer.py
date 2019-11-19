708acd9e4c19c81c40c20cedb649a95dfdccbc42
jeffery000 <31875986+jeffery000@users.noreply.github.com>
2019-11-19 21:31:02 +0800

    1

--- tutorials/01-basics/feedforward_neural_network/my_try_transfer.py
+++ tutorials/01-basics/feedforward_neural_network/my_try_transfer.py
@@ -4,9 +4,24 @@ import torch.nn as nn
 from torchvision import models
 
 #%%
-mbv = models.mobilenet_v2(pretrained=True)
+vgg16 = models.vgg16(pretrained=True)
+class VGGNet(nn.Module):
+    def __init__(self):
+        """Select conv1_1 ~ conv5_1 activation maps."""
+        super(VGGNet, self).__init__()
+        self.vgg = vgg16.features[:16]
+        
+    def forward(self, x):
+        """Extract multiple convolutional feature maps."""
+        features = []
+        for layer in self.vgg:
+            x = layer(x)
+            features.append(x)
+        return x
+
 
 #%%
-torch.save(mbv,"mvb.pth")
+my_vgg = VGGNet()
+torch.save(my_vgg,"vgg_b3_c3.pth")
 
 #%%
