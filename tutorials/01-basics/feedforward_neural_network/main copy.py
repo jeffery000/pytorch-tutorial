3faf6441e2fbb4a3e03eddb5cfa067bb22b7ffd5
jeffery000 <31875986+jeffery000@users.noreply.github.com>
2019-12-17 12:32:19 +0800

    1

--- tutorials/01-basics/feedforward_neural_network/main copy.py
+++ tutorials/01-basics/feedforward_neural_network/main copy.py
@@ -1,12 +1,14 @@
+#%%
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import time 
+import numpy as np
 
 # Device configuration
-device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
-print(device)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("using ",device)
 # Hyper-parameters 
 input_size = 784
 hidden_size = 500
@@ -38,12 +40,14 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.dropout = nn.Dropout(0.5) 
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)  
     
     def forward(self, x):
         out = self.fc1(x)
+        out = self.dropout(out)
         out = self.relu(out)
         out = self.fc2(out)
         return out
@@ -79,20 +83,31 @@ for epoch in range(num_epochs):
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
+#%%
+torch.save(model.state_dict(),"1.pth")
+#%%
+model2 = NeuralNet(input_size, hidden_size, num_classes).to(device)
+model2.load_state_dict(torch.load("1.pth"))
+model2.eval()
+losses = []
 with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
         images = images.reshape(-1, 28*28).to(device)
         labels = labels.to(device)
-        outputs = model(images)
+        outputs = model2(images)
+        loss = criterion(outputs, labels)
+        losses.append(loss.cpu().numpy())
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
     print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
-
+    print(np.mean(np.array(losses)))
 t2 = time.clock()
 print("using %s microsecond"%(t2-t1))
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
+
+# %%
