1604ef4ab0a796322f1628503d1c1ed0ecfdf54c
jeffery000 <31875986+jeffery000@users.noreply.github.com>
2020-03-15 10:42:41 +0800

    1

--- tutorials/01-basics/feedforward_neural_network/main_prediction.py
+++ tutorials/01-basics/feedforward_neural_network/main_prediction.py
@@ -1,14 +1,12 @@
-#%%
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-import time 
-import numpy as np
+
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print("using ",device)
+
 # Hyper-parameters 
 input_size = 784
 hidden_size = 500
@@ -40,74 +38,33 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.dropout = nn.Dropout(0.5) 
+        self.fc1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)  
     
     def forward(self, x):
         out = self.fc1(x)
-        out = self.dropout(out)
         out = self.relu(out)
         out = self.fc2(out)
         return out
 
 model = NeuralNet(input_size, hidden_size, num_classes).to(device)
-
+model.load_state_dict(torch.load("model.pth"))
 # Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
-
-# Train the model
-t1 = time.clock()
-
-total_step = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):  
-        # Move tensors to the configured device
-        images = images.reshape(-1, 28*28).to(device)
-        labels = labels.to(device)
-        
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
-#%%
-torch.save(model.state_dict(),"1.pth")
-#%%
-model2 = NeuralNet(input_size, hidden_size, num_classes).to(device)
-model2.load_state_dict(torch.load("1.pth"))
-model2.eval()
-losses = []
 with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
         images = images.reshape(-1, 28*28).to(device)
         labels = labels.to(device)
-        outputs = model2(images)
-        loss = criterion(outputs, labels)
-        losses.append(loss.cpu().numpy())
+        outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
     print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
-    print(np.mean(np.array(losses)))
-t2 = time.clock()
-print("using %s microsecond"%(t2-t1))
-# Save the model checkpoint
-torch.save(model.state_dict(), 'model.pth')
 
-# %%
+# Save the model checkpoint
+# torch.save(model.state_dict(), 'model.ckpt')
